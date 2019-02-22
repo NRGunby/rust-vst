fc8273d6ada1e2347076166e94bfcacb398920bd
Travis CI User <travis@example.org>
2019-02-22 01:26:27 +0000

    Update documentation

---
+++ source-files.js
@@ -0,0 +1,2 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex['vst'] = {"name":"","dirs":[],"files":["api.rs","buffer.rs","cache.rs","channels.rs","editor.rs","event.rs","host.rs","interfaces.rs","lib.rs","plugin.rs"]};
